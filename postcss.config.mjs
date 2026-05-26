007800bf3859f4c22c287366f32fa72490504f2c
Nat-intosh <54398543+Nat-intosh@users.noreply.github.com>
2026-05-26 15:54:38 +0200

    fix: remove postcss shit

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,6 +1,6 @@
-// postcss.config.mjs
-export default {
-  plugins: {
-    '@tailwindcss/postcss': {},
-  },
-}
+// // postcss.config.mjs
+// export default {
+//   plugins: {
+//     '@tailwindcss/postcss': {},
+//   },
+// }
