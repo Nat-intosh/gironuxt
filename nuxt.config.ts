63224a00a027aed318fe054fda4115091c7e9106
Nat-intosh <54398543+Nat-intosh@users.noreply.github.com>
2026-05-26 14:46:22 +0200

    feat: added back default strapi token

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -21,6 +21,6 @@ export default defineNuxtConfig({
   },
   strapi: {
     url: process.env.NUXT_PUBLIC_STRAPI_URL || 'http://localhost:1337',
-    token: process.env.NUXT_PUBLIC_STRAPI_TOKEN || "..."
+    token: process.env.NUXT_PUBLIC_STRAPI_TOKEN || "ba719062ee94fe36eaea5730a83f760c4154571738e9df24cb0d06053f46308e67136326a51e71f212a7f2318195dda816b1c806237aaced7b8c3a55692c3e943ef47fbdef08183edbe46d98cc85faab7208203d0c3522bc3eaadfbd38adb1191787b0709ee661926dba65074e13aebfc0379c3858f8b80201845ab8d3851075"
   },
 })
